9d79b62473bd14704ae4fc05e7f5499b16d45ce6
vvatanabe <yuichi.watanabe.ja@gmail.com>
2021-09-26 07:24:53 +0900

    fix creating credentials

--- sqs.go
+++ sqs.go
@@ -49,7 +49,7 @@ func Open(attrs map[string]interface{}) (*Connector, error) {
 
 	var awsCfg aws.Config
 	awsCfg.Region = aws.String(values.region)
-	if (values.accessKeyID != "" && values.secretAccessKey != "") || values.sessionToken != "" {
+	if values.accessKeyID != "" || values.secretAccessKey != "" || values.sessionToken != "" {
 		awsCfg.Credentials = credentials.NewStaticCredentials(
 			values.accessKeyID,
 			values.secretAccessKey,
